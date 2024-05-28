460a5c9c1d6b78ad7bec937c31d5b25aad9c11d1
Ty Byers <tyrone@wraphx.com>
2024-05-28 14:59:24 -0400

    Final Updates

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
         <Suspense fallback={null}>
         <Model3 scale={2.5} position={[0, -1.1, 0]} />
         </Suspense>
-        <hemisphereLight intensity={0.5} />
+        <hemisphereLight intensity={1} />
         <ContactShadows resolution={1024} frames={1} position={[0, -1.16, 0]} scale={15} blur={0.5} opacity={0.5} far={20} />
         <mesh scale={4} position={[3, -1.161, -1.5]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
           <ringGeometry args={[0.9, 1, 4, 1]} />
@@ -36,14 +36,14 @@ export default function App() {
           {/* Ceiling */}
           <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -9]} scale={[10, 1, 1]} />
           <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -6]} scale={[10, 1, 1]} />
-          <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -3]} scale={[10, 1, 1]} />
-          <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 0]} scale={[10, 1, 1]} />
-          <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 3]} scale={[10, 1, 1]} />
-          <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 6]} scale={[10, 1, 1]} />
-          <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 9]} scale={[10, 1, 1]} />
+          <Lightformer intensity={5} rotation-x={Math.PI / 2} position={[0, 4, -3]} scale={[10, 1, 1]} />
+          <Lightformer intensity={5} rotation-x={Math.PI / 2} position={[0, 4, 0]} scale={[10, 1, 1]} />
+          <Lightformer intensity={4} rotation-x={Math.PI / 2} position={[0, 4, 3]} scale={[10, 1, 1]} />
+          <Lightformer intensity={4} rotation-x={Math.PI / 2} position={[0, 4, 6]} scale={[10, 1, 1]} />
+          <Lightformer intensity={4} rotation-x={Math.PI / 2} position={[0, 4, 9]} scale={[10, 1, 1]} />
           {/* Sides */}
-          <Lightformer intensity={10} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} />
-          <Lightformer intensity={10} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} />
+          <Lightformer intensity={30} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} />
+          <Lightformer intensity={30} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} />
           {/* Key */}
           <Lightformer form="ring" color="gray" intensity={10} scale={2} position={[10, 5, 10]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
         </Environment>
